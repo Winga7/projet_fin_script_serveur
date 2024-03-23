13a246bd82007cc285db2bd02b1385e70642a1d8
Winga7 <andywinga@hotmail.be>
2024-03-23 15:12:08 +0100

    Mise à jour de la fonction de validation des formulaires***
    Ce commit met à jour la fonction de validation des formulaires pour améliorer la gestion des erreurs et la convivialité de l'interface utilisateur.

--- formulaire.php
+++ formulaire.php
@@ -1,6 +1,29 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    
+    $name = $_POST["name"];
+    $prenom = $_POST["prenom"];
+    $tel = $_POST["tel"];
+    $mail = $_POST["mail"];
+    $question = $_POST["question"];
+    $errors = [];
+    if (empty($name)){
+        $errors[] = "Le nom est obligatoire";
+    }
+    if (empty($prenom)){
+        $errors[] = "Le prénom est obligatoire";
+    }
+    if (empty($tel)){
+        $errors[] = "Le téléphone est obligatoire";
+    }
+    if (empty($mail)){
+        $errors[] = "Le mail est obligatoire";
+    }
+    if (empty($question)){
+        $errors[] = "La question est obligatoire";
+    }
+    if (empty($errors)){
+        $to = "
+    }
 }
 
 
