1270f595e4dcb53fa42a649bd9aadfcf48d044fa
Winga <andywinga@hotmail.be>
2024-05-31 16:40:13 +0200

    chore: Mise à jour du style CSS pour l'en-tête

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_URL',"/projet_fin_script_serveur");
+define('BASE_URL', "/projet_fin_script_serveur");
 
 function Navigationint()
 {
@@ -8,26 +8,30 @@ function Navigationint()
         BASE_URL . "/contact.php" => "Contact",
         BASE_URL . "/connexion.php" => "Connexion",
     ];
-    foreach ($pages as $page => $label){
-        $class = ($_SERVER['REQUEST_URI'] == $page) ?'active' : '';
-        echo '<li><a href="' . $page .'" class="' . $class . '">' . $label . '</a></li>';
+    foreach ($pages as $page => $label) {
+        $class = ($_SERVER['REQUEST_URI'] == $page) ? 'active' : '';
+        echo '<li><a href="' . $page . '" class="' . $class . '">' . $label . '</a></li>';
     }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?=$metaDesc?>">
+    <meta name="description" content="<?= $metaDesc ?>">
     <link rel="stylesheet" href="<?= BASE_URL ?> /public/assets/CSS/style.css">
     <link rel="stylesheet" href="<?= BASE_URL ?> /public/assets/CSS/styleform.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="icon" type="image/svg+xml" href="public/images/Winga.jpg" />
     <title>Mon Premier modèle Dynamique</title>
 </head>
+
 <body>
-<header>
+    <header>
         <nav>
             <ul>
                 <?= Navigationint() ?>
