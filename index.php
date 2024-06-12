c7e1d74236a23707d5f165cbd774b5e0b4e1c09c
Winga7 <andywinga@hotmail.be>
2024-06-12 14:19:41 +0200

    feat: Ajouter une galerie d'images sur la page d'accueil

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 
     <main>
         <h1>Page Accueil</h1>
+        <p>Bienvenue sur mon premier site en php bien que je ne maitrise pas ce language j'espère que tu trouvera ce site "correct" je ne m'attend pas à etre le plus beau site loin de là mais du moins qu'il sois fonctionnel</p>
+        <ul id="galery">
+            <li><img src="/public/images/istockphoto-157506243-612x612.jpg" alt="ISS by Scibak" title="ISS by Scibak"></li>
+            <li><img src="/public/images/149627992-universe-scene-with-planets.jpg" alt="Univers"></li>
+            <li><img src="/public/images/lune_terre.jpg" alt="lune/terre"></li>
+            <li><img src="/public/images/istockphoto-692243858-612x612.jpg" alt=""></li>
+        </ul>
+        <p>Je te souhaite une bonne visite</p>
     </main>
 
     <?php
